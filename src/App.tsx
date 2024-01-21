4f839394f622a908832ac5283c71058ae71641d2
Roman <usankin.roma@gmail.com>
2024-01-22 00:47:23 +0400

    work in task

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
     const [open, setOpen] = useState(false)
+    const [seconds, setSeconds] = useState(5);
+    const [timerActive, setTimerActive] = useState(false);
+    const [disabled, setDisabled] = useState(true);
+    const [isConfirm, setIsConfirm] = useState(false)
     const openModal = () => {
-        setOpen(true)
+        if (!isConfirm) {
+            setOpen(true)
+            setTimerActive(true)
+        } else {
+            alert('Действие выполнено!')
+        }
     }
 
     const closeModal = () => {
         setOpen(false)
+        setSeconds(5)
+        setTimerActive(false)
     }
 
     const confirm = () => {
+
         setOpen(false)
+        alert('Действие выполнено')
+        setIsConfirm(true)
+
     }
+
+    useEffect(() => {
+        if (seconds > 0 && timerActive) {
+            setTimeout(setSeconds, 1000, seconds - 1);
+            setDisabled(true)
+        } else {
+            setTimerActive(false);
+            setDisabled(false)
+        }
+    }, [seconds, timerActive]);
 
     return (
         <div className="App">
             <button onClick={openModal}>Выполнить действие</button>
+            <div>isConfirm: {isConfirm}</div>
+            <div>seconds: {seconds}</div>
             {
                 open && (
                     <>
@@ -40,8 +67,9 @@ function App() {
                                     <button
                                         className="modal__button btn btn--green"
                                         onClick={confirm}
+                                        disabled={disabled}
                                     >
-                                        Подтвердить
+                                        Подтвердить {seconds > 0 ? `(${seconds})` : ''}
                                     </button>
                                 </div>
                             </div>
