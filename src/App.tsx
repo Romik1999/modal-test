1dd1aafdec4e7e60be4ae9ee69fc75d65ae7de56
Roman <usankin.roma@gmail.com>
2024-01-22 00:22:41 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [open, setOpen] = useState(false)
+    const openModal = () => {
+        setOpen(true)
+    }
+
+    const closeModal = () => {
+        setOpen(false)
+    }
+
+    const confirm = () => {
+        setOpen(false)
+    }
+
+    return (
+        <div className="App">
+            <button onClick={openModal}>Выполнить действие</button>
+            {
+                open && (
+                    <>
+                        <div className="fade"></div>
+                        <div className="modal">
+                            <div className="modal__content">
+                                <div className="modal__title">Согласие с правилами</div>
+                                <div className="modal__closer" onClick={closeModal}></div>
+                                <div className="modal__text">
+                                    Для данной функции применяются особые условия и правила пользования, их необходимо
+                                    подтвердить, нажав на кнопку Подтвердить»
+                                </div>
+                                <div className="modal__buttons">
+                                    <button
+                                        className="modal__button btn btn--red"
+                                        onClick={closeModal}
+                                    >
+                                        Отмена
+                                    </button>
+                                    <button
+                                        className="modal__button btn btn--green"
+                                        onClick={confirm}
+                                    >
+                                        Подтвердить
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
